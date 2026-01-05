822afa75adce574435c91e6aba78a75f01f27f0e
Greg Harvey <gregsharvey@gmail.com>
2026-01-05 15:30:25 -0500

    Update context.py

--- context.py
+++ context.py
@@ -2,5 +2,4 @@ from pathlib import Path
 
 _dir = Path(__file__).parent
 
-def get(name="default"):
-    return (_dir / f"{name}.md").read_text()
+def get(name="default"): return (_dir / f"{name}.md").read_text()
